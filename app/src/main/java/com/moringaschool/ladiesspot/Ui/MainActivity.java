5222cce0e74f6ce83c34c58f29832a1893a2d748
SheinaHamisi <hamisisheina@gmail.com>
2022-04-06 11:46:17 +0300

    updated main activity

--- app/src/main/java/com/moringaschool/ladiesspot/Ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/ladiesspot/Ui/MainActivity.java
@@ -2,15 +2,34 @@ package com.moringaschool.ladiesspot.Ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.moringaschool.ladiesspot.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.button) Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == button){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
